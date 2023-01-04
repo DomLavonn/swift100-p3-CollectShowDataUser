c3328145713446fd7065e81ec8129e51352d8655
DomLavonn <110582412+Domanikl@users.noreply.github.com>
2023-01-03 19:17:48 -0500

    Initial Commit

---
+++ selectorform/selectorformApp.swift
@@ -0,0 +1,17 @@
+//
+//  selectorformApp.swift
+//  selectorform
+//
+//  Created by Domanik Johnson on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct selectorformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
